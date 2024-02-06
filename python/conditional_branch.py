29458aaf9bf0549198d21b43a03286bae9c86157
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-02-06 22:37:28 +0900

    分岐とif文を追加しました

--- python/conditional_branch.py
+++ python/conditional_branch.py
@@ -1,3 +1,6 @@
+# 分岐
+# if文
+
 number = input("Enter a number: ")
 number = int(number)
 
@@ -9,7 +12,7 @@ else:
     print("The number is between 10 and 100.")
 
 # switch文はない
-# 代わりにdictionaryを使う
+# 代わりにdictionaryを使うけど，　同じことはできない
 dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"}
 number = number % 10
 
