973e953828e5ef3b3f415a5c14b6bd899fd0fe8f
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-02-06 14:58:58 +0900

    ok hello

--- ans/hello.c
+++ ans/hello.c
@@ -5,10 +5,14 @@
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
     printf("Hello, World!\n");
+    fprintf(stdout, "Hello, World!\n");
+    puts("Hello, World!");
+    write(1, "Hello, World!\n", 14);
     
     return 0;
 }
