d312d8e6562b90174ba01e39d6e08c75a346606f
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-02-06 15:08:35 +0900

    変数と型に関するコードの変更を追加

---
+++ python/variable.py
@@ -0,0 +1,11 @@
+# 変数
+
+num = 335  # 整数
+pi = 3.14  # 浮動小数点
+name = "Python"  # 文字列
+is_ok = True  # 真偽値
+
+print(num, type(num))
+print(pi, type(pi))
+print(name, type(name))
+print(is_ok, type(is_ok))
