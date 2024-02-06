d99b7fb3894fa86d1170c8b1dca9b925b0caedf2
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-02-06 22:27:17 +0900

    演算を学習するプログラムに関する変更を追加

---
+++ python/calculation.py
@@ -0,0 +1,36 @@
+# 演算
+
+a = 101
+b = 23
+
+print('a + b:', a + b)  # 足し算
+print('a - b:', a - b)  # 引き算
+print('a * b:', a * b)  # 掛け算
+print('a / b:', a / b)  # 割り算
+print('a % b:', a % b)  # 余り
+print('a // b:', a // b)  # 商
+print('a ** b:', a ** b)  # べき乗
+
+print('a++:', a + 1)  # インクリメント
+print('a--:', a - 1)  # デクリメント
+
+print('a == b:', a == b)  # 等しい
+print('a != b:', a != b)  # 等しくない
+print('a > b:', a > b)  # より大きい
+print('a < b:', a < b)  # より小さい
+print('a >= b:', a >= b)  # 以上
+print('a <= b:', a <= b)  # 以下
+
+print('a and b:', a and b)  # 論理積
+print('a or b:', a or b)  # 論理和
+print('not a:', not a)  # 否定
+print('a in b:', a in b)  # 含まれる
+print('a not in b:', a not in b)  # 含まれない
+print('a is b:', a is b)  # 同じ
+print('a is not b:', a is not b)  # 同じでない
+print('a & b:', a & b)  # ビット積
+print('a | b:', a | b)  # ビット和
+print('a ^ b:', a ^ b)  # 排他的論理和
+print('a << 2:', a << 2)  # 左シフト
+print('a >> 2:', a >> 2)  # 右シフト
+print('~a:', ~a)  # ビット反転
