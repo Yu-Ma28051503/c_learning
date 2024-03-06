af4ec96e06533b3057aac025f66931511100eb6f
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-03-07 01:35:39 +0900

    変数の型を出力するように修正

--- python/variable.py
+++ python/variable.py
@@ -5,10 +5,10 @@ pi = 3.14  # 浮動小数点
 name = "Python"  # 文字列
 is_ok = True  # 真偽値
 
-print(num, type(num))
-print(pi, type(pi))
-print(name, type(name))
-print(is_ok, type(is_ok))
+print(num, type(num))  # int
+print(pi, type(pi))  # float
+print(name, type(name))  # str
+print(is_ok, type(is_ok))  # bool
 
 # global変数はglobal_の後に変数名をつけることで定義
 global_num = 10 # グローバル変数
