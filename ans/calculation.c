cc9f4f4d64316d96f8ce0bea5d7bea33e0cec6cd
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2024-03-07 01:35:44 +0900

    四則演算、型変換、代入演算、比較演算、論理演算を追加

--- ans/calculation.c
+++ ans/calculation.c
@@ -10,23 +10,34 @@ int main(void)
 {
     int a = 101, b = 23, c = 0;
 
+    // 四則演算
     printf("a + b = %d\n", a + b);  // 足し算
     printf("a - b = %d\n", a - b);  // 引き算
     printf("a * b = %d\n", a * b);  // 掛け算
     printf("a / b = %d\n", a / b);  // 割り算
     printf("a %% b = %d\n", a % b);  // 剰余
 
+    // 型変換
     printf("(int)a / (int)b = %d\n", (int)a / (int)b);  // キャスト(型変換)
     printf("(double)a / (int)b = %f\n", (double)a / (int)b);  // キャスト(型変換)
     printf("(int)a / (double)b = %f\n", (int)a / (double)b);  // キャスト(型変換)
     printf("(double)a / (double)b = %f\n", (double)a / (double)b);  // キャスト(型変換)
 
+    // 代入演算
     printf("a += b: %d\n", a += b);  // a = a + b
+    a = 101; b = 23;
     printf("a -= b: %d\n", a -= b);  // a = a - b
+    a = 101; b = 23;
     printf("a *= b: %d\n", a *= b);  // a = a * b
+    a = 101; b = 23;
     printf("a /= b: %d\n", a /= b);  // a = a / b
+    a = 101; b = 23;
     printf("a %%= b: %d\n", a %= b);  // a = a % b
 
+    // 値を初期値の戻す
+    a = 101; b = 23;
+
+    // インクリメント・デクリメント
     printf("a++: %d\n", a++);  // a = a + 1
     printf("a--: %d\n", a--);  // a = a - 1
     printf("++a: %d\n", ++a);  // a = a + 1
@@ -36,6 +47,7 @@ int main(void)
     printf("c = a + b: %d\n", c = a + b);  // a + b を c に代入
     printf("c: %d\n", c);
 
+    // 比較演算
     printf("a == b: %d\n", a == b);  // a と b が等しい
     printf("a != b: %d\n", a != b);  // a と b が等しくない
     printf("a > b: %d\n", a > b);  // a が b より大きい
@@ -43,6 +55,7 @@ int main(void)
     printf("a >= b: %d\n", a >= b);  // a が b 以上
     printf("a <= b: %d\n", a <= b);  // a が b 以下
     
+    // 論理演算
     printf("a&b: %d\n", a&b);  // a と b の論理積
     printf("a|b: %d\n", a|b);  // a と b の論理和
     printf("a^b: %d\n", a^b);  // a と b の排他的論理和
